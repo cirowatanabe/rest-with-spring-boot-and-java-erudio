3e74536edeea6008f0f9764b9d9589ca265b965b
Ciro Watanabe <cw.artiga@gmail.com>
2024-09-24 09:39:35 -0300

    adicionada migration ao projeto

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Sao Paulo - Brasil', 'ayrton', 'Male', 'Senna'),
+	(2, 'Uberlandia - Minas Gerais - Brasil', 'Leandro', 'Male', 'da Silva'),
+	(4, 'Uberlandia - Minas Gerais - Brasil', 'Leandro', 'Male', 'da Silva'),
+	(6, 'Planalto - Minas Gerais - Brasil', 'Marcia', 'Female', 'da Silva'),
+	(7, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(8, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
